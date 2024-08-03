f7478d48218c6d99fa58883face06599c77d2b81
Praveenkumar <166112831+PRAVEENKUMARhicet@users.noreply.github.com>
2024-08-03 11:17:34 +0530

    Update script.js
    make some additional validation logics

--- script.js
+++ script.js
@@ -1,92 +1,91 @@
 var marks = document.getElementsByClassName('mark');
 var total = document.getElementById('percentage');
-var totalmark=document.getElementById('grandtotal');
-var word=document.getElementsByClassName('words');
-var result=document.getElementById('grade');
+var totalmark = document.getElementById('grandtotal');
+var word = document.getElementsByClassName('words');
+var result = document.getElementById('grade');
+var resulttype=document.getElementById('result');
 var totalmarks = 0;
 
-for (let i = 0; i < marks.length; i++) {
+for (let i = 0; i < marks.length; i++){
     marks[i].addEventListener('keyup', update);
 }
 
 function update() {
     totalmarks = 0;
-    var subject = document.getElementsByClassName('subject-mark');
-
-    for (let i = 0; i < marks.length; i++) {
-        if(marks[i]>100)
-            alert("enter a valid mark");
+    var subject=document.getElementsByClassName('subject-mark');
+    
+    for (let i = 0; i < marks.length; i++){
         var mark = parseFloat(marks[i].value) || 0;
+        if (mark > 100) {
+            alert("Enter a valid mark (0-100)");
+            marks[i].value = '';
+            mark = 0;
+        }
+        subject[i].innerHTML=mark;
         totalmarks += mark;
-        subject[i].innerHTML = mark;
+        word[i].innerHTML = words(mark);
     }
-    words(totalmarks);
+    word[word.length-1].innerHTML=words(totalmarks);
     grandtotal(totalmarks);
     percentage(totalmarks);
 }
 
-function percentage(totalmarks) {
-    let percentage = marks.length > 0 ? totalmarks / marks.length : 0;
-    total.innerHTML = percentage.toFixed(2)+"%";
+function percentage(totalmarks){
+    let percentage = marks.length > 0 ? (totalmarks / (marks.length * 100)) * 100 : 0;
+    total.innerHTML = percentage.toFixed(2) + "%";
+    if(percentage<35)
+        resulttype[0].innerHTML="Fail";
+    else    
+        resulttype.innerHTML[0]="Pass";
+
     grade(percentage);
 }
 
 function grandtotal(totalmarks){
-    totalmark.innerHTML=totalmarks;
+    totalmark.innerHTML = totalmarks;
 }
 
-function words(num) {
+function words(num){
     if (num === 0) 
         return "zero";
-
     const ones = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
     const tens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
     const hundreds = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
-    const thousands = ["", "thousand", "million", "billion"];
-    let words = "";
-    let i = 0;
-
-    while (num > 0) {
-        let rem = num % 1000;
-        if (rem > 0) {
-            words = convertHundreds(rem) + " " + thousands[i] + " " + words;
-        }
-        num = Math.floor(num / 1000);
-        i++;
-    }
     
-    function convertHundreds(num) {
-        let result = "";
+    function converthundreds(num){
+        let result1 = "";
         if (num > 99) {
-            result += ones[Math.floor(num / 100)] + " hundred ";
+            result1 += ones[Math.floor(num / 100)] + " hundred ";
             num %= 100;
         }
         if (num > 19) {
-            result += hundreds[Math.floor(num / 10)] + " ";
+            result1 += hundreds[Math.floor(num / 10)] + " ";
             num %= 10;
         }
         if (num > 9) {
-            result += tens[num - 10] + " ";
+            result1 += tens[num - 10] + " ";
         } else if (num > 0) {
-            result += ones[num] + " ";
+            result1 += ones[num] + " ";
         }
-        return result;
+        return result1;
     }
-    console.log(words);
-    for(let i=0;i<word.length;i++)
-        word[i].innerHTML=words;
+
+    return converthundreds(num);
 }
 
-function grade(percentage){
-    var res="";
-    if(percentage>=90 && percentage<=100){
-        res="A";
+function grade(percentage) {
+    var res = "";
+    if (percentage >= 90) {
+        res = "A";
+    } 
+    else if (percentage >= 60) {
+        res = "B";
+    } 
+    else if (percentage >= 35) {
+        res = "C";
+    } 
+    else if (percentage<35 && percentage>=0) {
+        res = "F";
     }
-    else if(percentage>=60 && percentage<90)
-        res="B"
-    else if(percentage>=35 && percentage<60)
-        res="c";
-    else
-        res="F";
-    result.innerHTML=res;
+    result.innerHTML = res;
 }
